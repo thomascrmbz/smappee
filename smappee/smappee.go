e8b5e5446304759e34793594473a6fbef618277d
Thomas Crombez <thomas@trikthom.com>
2021-03-27 10:52:55 +0100

    Added smappee type

--- smappee/smappee.go
+++ smappee/smappee.go
@@ -1,1 +1,22 @@
 package smappee
+
+import "net/http"
+
+type Smappee struct {
+	ClientID     string
+	ClientSecret string
+	Username     string
+	Password     string
+
+	client *http.Client
+}
+
+func NewSmappee(clientID string, clientSecret string, username string, password string) *Smappee {
+	return &Smappee{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Username:     username,
+		Password:     password,
+		client:       http.DefaultClient,
+	}
+}
