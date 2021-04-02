d546a1c364fca3d89e1a2d95b31def57165a6f51
Thomas Crombez <thomas@trikthom.com>
2021-04-02 17:44:36 +0200

    Added convert methods

--- api.go
+++ api.go
@@ -32,21 +32,7 @@ func (s *Smappee) GetServiceLocation(id int) (ServiceLocation, error) {
 	sli := serviceLocationInfoResponse{}
 	json.NewDecoder(res.Body).Decode(&sli)
 
-	return ServiceLocation{
-		Name:                sli.Name,
-		UUID:                sli.UUID,
-		ID:                  sli.ID,
-		DeviceSerialNumber:  sli.DeviceSerialNumber,
-		Latitude:            sli.Latitude,
-		Longtitude:          sli.Longtitude,
-		ElectricityCost:     sli.ElectricityCost,
-		ElectricityCurrency: sli.ElectricityCurrency,
-		Timezone:            sli.Timezone,
-
-		ChannelsConfiguration: sli.ChannelsConfiguration,
-
-		From: time.Unix(0, sli.From*int64(time.Millisecond)),
-	}, err
+	return convertServiceLocation(sli), err
 }
 
 func (s *Smappee) DeleteServiceLocation(id int) error {
@@ -87,27 +73,7 @@ func (s *Smappee) GetElectricityConsumption(id int, timestamp ...time.Time) (Ele
 	i := len(electricityConsumptionsResponse.Consumptions) - 1
 	c := electricityConsumptionsResponse.Consumptions[i]
 
-	return ElectricityConsumption{
-		Timestamp:       time.Unix(0, c.Timestamp*int64(time.Millisecond)),
-		ConsumptionWh:   c.Consumption,
-		ConsumptionW:    c.Consumption * 12,
-		SolarWh:         c.Solar,
-		SolarW:          c.Solar * 12,
-		AlwaysOnWh:      c.AlwaysOn,
-		AlwaysOnW:       c.AlwaysOn * 12,
-		GridImportWh:    c.GridImport,
-		GridImportW:     c.GridImport * 12,
-		GridExportWh:    c.GridExport,
-		GridExportW:     c.GridExport * 12,
-		SelfSufficiency: c.SelfSufficiency,
-		SelfConsumption: c.SelfConsumption,
-		ActiveWh:        sum(c.Active),
-		ActiveW:         round(sum(c.Active) * 12),
-		ReactiveWh:      sum(c.Reactive),
-		ReactiveW:       round(sum(c.Reactive) * 12),
-		Voltages:        c.Voltages,
-		Current:         sum(c.Current),
-	}, err
+	return convertElectricityConsumption(c), err
 }
 
 func (s *Smappee) GetElectricityConsumptions(aggregation int, from time.Time, to ...time.Time) ([]ElectricityConsumption, error) {
---
+++ convert.go
@@ -0,0 +1,45 @@
+package smappee
+
+import "time"
+
+func convertServiceLocation(sli serviceLocationInfoResponse) ServiceLocation {
+	return ServiceLocation{
+		Name:                sli.Name,
+		UUID:                sli.UUID,
+		ID:                  sli.ID,
+		DeviceSerialNumber:  sli.DeviceSerialNumber,
+		Latitude:            sli.Latitude,
+		Longtitude:          sli.Longtitude,
+		ElectricityCost:     sli.ElectricityCost,
+		ElectricityCurrency: sli.ElectricityCurrency,
+		Timezone:            sli.Timezone,
+
+		ChannelsConfiguration: sli.ChannelsConfiguration,
+
+		From: time.Unix(0, sli.From*int64(time.Millisecond)),
+	}
+}
+
+func convertElectricityConsumption(c electricityConsumptionResponse) ElectricityConsumption {
+	return ElectricityConsumption{
+		Timestamp:       time.Unix(0, c.Timestamp*int64(time.Millisecond)),
+		ConsumptionWh:   c.Consumption,
+		ConsumptionW:    c.Consumption * 12,
+		SolarWh:         c.Solar,
+		SolarW:          c.Solar * 12,
+		AlwaysOnWh:      c.AlwaysOn,
+		AlwaysOnW:       c.AlwaysOn * 12,
+		GridImportWh:    c.GridImport,
+		GridImportW:     c.GridImport * 12,
+		GridExportWh:    c.GridExport,
+		GridExportW:     c.GridExport * 12,
+		SelfSufficiency: c.SelfSufficiency,
+		SelfConsumption: c.SelfConsumption,
+		ActiveWh:        sum(c.Active),
+		ActiveW:         round(sum(c.Active) * 12),
+		ReactiveWh:      sum(c.Reactive),
+		ReactiveW:       round(sum(c.Reactive) * 12),
+		Voltages:        c.Voltages,
+		Current:         sum(c.Current),
+	}
+}
